e66cee5e8d80c6c926d4d3b03d6160ca5b3b2bb5
Nikhil Murthy <murthynik1@gmail.com>
2016-03-20 14:11:21 +0530

    Allow underscores in parser.

--- parseCsv.py
+++ parseCsv.py
@@ -9,7 +9,7 @@ def parseCsvToken(st):
     floatNum = pp.Combine(pp.Optional('-') + pp.Word(pp.nums) + '.' + pp.Word(pp.nums))
     floatNum.setParseAction(lambda toks: float(toks[0]))
     
-    identifier = pp.Word(pp.alphanums + ' -')
+    identifier = pp.Word(pp.alphanums + ' -_')
     basicToken = num ^ floatNum ^ identifier
 
     lst = pp.Forward()
