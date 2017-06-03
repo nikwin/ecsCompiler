681776956369dccfbdea33a3ad5482882a02f7ab
Nikhil Murthy <murthynik1@gmail.com>
2017-06-03 20:22:49 +0530

    Powered up the list rows script.

--- listRows.py
+++ listRows.py
@@ -1,8 +1,14 @@
 import csv
 import os
 
-def listRows(folder, column):
-    files = os.listdir(folder)
+import click
+
+@click.command()
+@click.argument('foldername')
+@click.argument('columns', nargs=-1)
+@click.option('--flatten', is_flag=True, default=False)
+def listRows(foldername, columns, flatten):
+    files = os.listdir(foldername)
     pairs = []
     
     for fil in files:
@@ -10,25 +16,28 @@ def listRows(folder, column):
         if ext != '.csv':
             continue
                     
-        with open(os.path.join(folder, fil)) as f:
-            columnNumber = -1
-            try:
-                for i, row in enumerate(csv.reader(f)):
-                    if not row: 
-                        continue
-                    if i == 0:
-                        columnNumber = row.index(column)
-                    else:
-                        pairs.append((row[0], row[columnNumber]))
-            except ValueError:
-                pass
-
-    longestKey = max(len(pair[0]) for pair in pairs)
-    for pair in pairs:
-        print ','.join(pair)
+        with open(os.path.join(foldername, fil)) as f:
+            columnNumbers = False
+            for i, row in enumerate(csv.reader(f)):
+                if not row: 
+                    continue
+                if i == 0:
+                    columnNumbers = [row.index(column) for column in columns if column in row]
+                    if not columnNumbers:
+                        break
+                    columnNumbers = [0] + columnNumbers
+                else:
+                    pairs.append([row[columnNumber] for columnNumber in columnNumbers if row[columnNumber]])
+            
+    if flatten:
+        elements = []
+        for pair in pairs:
+            for ele in pair[1:]:
+                elements.append(ele)
+        print ' '.join(elements)
+    else:
+        for pair in pairs:
+            print ','.join(pair)
 
 if __name__ == '__main__':
-    import sys
-    folder = sys.argv[1]
-    column = sys.argv[2]
-    listRows(folder, column)
+    listRows()
